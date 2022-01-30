5656482867dce6475cf53f93d8212134d8a60745
Raeye Jordan <raeyejordaan@yahoo.com>
2022-01-29 23:23:25 -0800

    removed unnecessary styles

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { ImageBackground } from 'react-native';
 import Login from './screens/Login';
 import Account from './screens/Account';
 import { useUser } from './UserManager';
 
 const image = require('../public/logo.png');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E2F0FB',
-  },
-});
-
 function Home() {
   const { subscribed } = useUser();
 
   return (
-    <ImageBackground source={image} style={styles.container}>
+    <ImageBackground source={image}>
       {!subscribed ? (<Login />) : (<Account />)}
     </ImageBackground>
   );
