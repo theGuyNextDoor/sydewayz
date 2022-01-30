ef6a7c02dfde989052b46e89398461e2d2f64dd2
Raeye Jordan <raeyejordaan@yahoo.com>
2022-01-29 22:15:39 -0800

    added bare minimum styling for Form.js

--- client/public/styles/Form.jsx
+++ client/public/styles/Form.jsx
@@ -4,40 +4,70 @@ const styles = StyleSheet.create({
   wrapper: {
     width: '100%',
     height: '100%',
-    paddingTop: '10%',
-    paddingBottom: '10%',
+    borderRadius: 30,
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#26C8D2',
   },
   container: {
-    width: '80%',
-    // height: '100%',
+    width: '90%',
+    height: '80%',
+    justifyContent: 'space-between',
 
-    borderWidth: 1,
+    // borderWidth: 1,
   },
+
+  // SUBJECT
   subjectContainer: {
+    flex: 3,
+
+    // borderWidth: 1,
+  },
+  subjectHeaderContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: '2%',
-
-    borderWidth: 1,
   },
   subjectBox: {
-    height: '10%',
-    marginBottom: '10%',
+    height: '40%',
+    paddingRight: '5%',
+    paddingLeft: '5%',
+    borderRadius: 10,
     backgroundColor: '#FFF',
+    marginBottom: '2%',
 
-    borderWidth: 1,
+    // borderWidth: 1,
+  },
+
+  // DESCRIPTION
+  descriptionContainer: {
+    flex: 6,
+
+    // borderWidth: 1,
   },
   descriptionBox: {
-    height: '30%',
+    height: '70%',
+    paddingTop: '3%',
+    paddingRight: '5%',
+    paddingBottom: '3%',
+    paddingLeft: '5%',
+    borderRadius: 10,
     backgroundColor: '#FFF',
+    marginBottom: '2%',
 
-    borderWidth: 1,
+    // borderWidth: 1,
   },
-  buttonContainer: {
 
-    borderWidth: 1,
+  // BUTTONS
+  btnContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+    // borderWidth: 1,
+  },
+  errorMsg: {
+    color: '#FF0000',
   },
 });
 
--- client/src/screens/Form.jsx
+++ client/src/screens/Form.jsx
@@ -1,101 +1,12 @@
 import React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
+import { useUser } from '../UserManager';
 import theme from '../../public/theme';
 import styles from '../../public/styles/Form';
 
-// function Form({ handleModalView }) {
-//   const {
-//     control,
-//     handleSubmit,
-//     reset,
-//     formState: { errors },
-//   } = useForm({
-//     defaultValues: {
-//       subject: '',
-//       description: '',
-//       email: '',
-//     },
-//   });
-
-//   const onSubmit = (form) => {
-//     reset({
-//       subject: '',
-//       description: '',
-//     });
-//     console.log(form);
-//   };
-
-//   return (
-//     <View style={styles.wrapper}>
-//       <View style={styles.subjectContainer}>
-
-//         <View style={styles.subjectHeader}>
-//           <Text>subject:</Text>
-//           <Text onPress={() => handleModalView('none')}>X</Text>
-
-//         </View>
-
-//         <View style={styles.subjectInputContainer}>
-//           <Controller
-//             control={control}
-//             rules={{ required: true, maxLength: 20 }}
-//             render={({ field: { onChange, onBlur, value } }) => (
-//               <TextInput
-//                 style={styles.subjectInput}
-//                 onBlur={onBlur}
-//                 onChangeText={onChange}
-//                 value={value}
-//               />
-//             )}
-//             name="subject"
-//           />
-//           {errors.subject && <Text>error subject uncomment for production</Text>}
-//           {/* {errors.subject?.type === 'required' && <Text>subject is required</Text>} */}
-//           {/* {errors.subject?.type === 'maxLength' && <Text>subject cannot exceed 20</Text>} */}
-//         </View>
-//       </View>
-//       <View style={styles.descriptionContainer}>
-//         <Text>description:</Text>
-//         <Controller
-//           control={control}
-//           rules={{ required: true, maxLength: 200 }}
-//           render={({ field: { onChange, onBlur, value } }) => (
-//             <TextInput
-//               style={styles.descriptionInput}
-//               onBlur={onBlur}
-//               onChangeText={onChange}
-//               value={value}
-//             />
-//           )}
-//           name="description"
-//         />
-//         {errors.description && <Text>error description uncomment for production</Text>}
-//         {/* {errors.description?.type === 'required' && <Text>description is required</Text>} */}
-//         {/* {errors.description?.type === 'maxLength' && <Text>description cannot exceed 200</Text>} */}
-//       </View>
-
-//       <Button
-//         title="Clear"
-//         onPress={() => {
-//           reset({
-//             subject: '',
-//             description: '',
-//           });
-//         }}
-//       />
-
-//       <Button
-//         title="Submit"
-//         onPress={handleSubmit(onSubmit)}
-//       />
-//     </View>
-//   );
-// }
-
-// export default Form;
-
 function Form({ handleModalView }) {
+  const { user } = useUser();
   const {
     control,
     handleSubmit,
@@ -105,16 +16,16 @@ function Form({ handleModalView }) {
     defaultValues: {
       subject: '',
       description: '',
-      email: '',
+      email: user.email,
     },
   });
 
-  const onSubmit = (form) => {
+  const onSubmit = (data) => {
     reset({
       subject: '',
       description: '',
     });
-    console.log(form);
+    console.log(data);
   };
 
   return (
@@ -122,48 +33,62 @@ function Form({ handleModalView }) {
       <View style={styles.container}>
 
         <View style={styles.subjectContainer}>
-          <Text>subject:</Text>
-          <Text onPress={() => handleModalView('none')}>X</Text>
+          <View style={styles.subjectHeaderContainer}>
+            <Text>subject:</Text>
+            <Text onPress={() => handleModalView('none')}>X</Text>
+          </View>
+          <Controller
+            control={control}
+            rules={{ required: true, maxLength: 20 }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.subjectBox}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="subject"
+          />
+          <Text style={styles.errorMsg}>Here is the error</Text>
+
+          {/* {errors.subject?.type === 'required' && (
+            <Text style={styles.errorMsg}>subject is required</Text>
+          )}
+          {errors.subject?.type === 'maxLength' && (
+            <Text style={styles.errorMsg}>subject cannot exceed 20</Text>
+          )} */}
         </View>
 
-        <Controller
-          control={control}
-          rules={{ required: true, maxLength: 20 }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.subjectBox}
-              // multiline="true"
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
+        <View style={styles.descriptionContainer}>
+          <Text style={{ marginBottom: '2%' }}>description:</Text>
+
+          <Controller
+            control={control}
+            rules={{ required: true, maxLength: 200 }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                multiline
+                style={styles.descriptionBox}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="description"
+          />
+          <Text style={styles.errorMsg}>Here is the error</Text>
+
+          {/* {errors.description?.type === 'required' && (
+            <Text style={styles.errorMsg}>description is required</Text>
           )}
-          name="subject"
-        />
-        {errors.subject && <Text>error subject uncomment for production</Text>}
-        {/* {errors.subject?.type === 'required' && <Text>subject is required</Text>} */}
-        {/* {errors.subject?.type === 'maxLength' && <Text>subject cannot exceed 20</Text>} */}
 
-        <Text>description:</Text>
+          {errors.description?.type === 'maxLength' && (
+            <Text style={styles.errorMsg}>description cannot exceed 200</Text>
+          )} */}
+        </View>
 
-        <Controller
-          control={control}
-          rules={{ required: true, maxLength: 200 }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.descriptionBox}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-          )}
-          name="description"
-        />
-        {errors.description && <Text>error description uncomment for production</Text>}
-        {/* {errors.description?.type === 'required' && <Text>description is required</Text>} */}
-        {/* {errors.description?.type === 'maxLength' && <Text>description cannot exceed 200</Text>} */}
-
-        <View styles={styles.buttonContainer}>
+        <View style={styles.btnContainer}>
           <Button
             title="Clear"
             onPress={() => {
